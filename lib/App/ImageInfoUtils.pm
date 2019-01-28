e35c25d94688c2488c372db0736e910c7aee6e81
perlancar (on pc-office) <perlancar@gmail.com>
2019-01-28 20:14:20 +0700

    Release v0.001

--- lib/App/ImageInfoUtils.pm
+++ lib/App/ImageInfoUtils.pm
@@ -37,13 +37,13 @@ $SPEC{image_info} = {
     },
 };
 sub image_info {
-    require Media::Info;
+    require Image::Info;
 
     my %args = @_;
 
     my @rows;
     for my $file (@{$args{files}}) {
-        my $res = image_info($file);
+        my $res = Image::Info::image_info($file);
         if ($res->{error}) {
             warn "Can't get image info for '$file': $res->{error}\n";
             next;
@@ -64,9 +64,6 @@ $SPEC{image_is_portrait} = {
     summary => 'Return exit code 0 if image is portrait',
     description => <<'_',
 
-Portrait is defined as having 'rotate' metadata of 90 or 270 when the width >
-height. Otherwise, media is assumed to be 'landscape'.
-
 _
     args => {
         %arg0_file,
@@ -75,25 +72,23 @@ _
 sub image_is_portrait {
     my %args = @_;
 
-    my $res = image_info(file => [$args{file}]);
+    my $res = image_info(files => [$args{file}]);
     return $res unless $res->[0] == 200;
 
-    my $rotate = $res->[2]{rotate} // 0;
-    my $width  = $res->[2]{video_width}  // $res->[2]{width};
-    my $height = $res->[2]{video_height} // $res->[2]{height};
-    return [412, "Can't determine video width x height"] unless $width && $height;
-    my $is_portrait = ($rotate == 90 || $rotate == 270 ? 1:0) ^ ($width <= $height ? 1:0) ? 1:0;
+    my $orientation = $res->[2]{Orientation} // '';
+    my $width  = $res->[2]{width};
+    my $height = $res->[2]{height};
+    return [412, "Can't determine image orientation"] unless $orientation;
+    return [412, "Can't determine image width x height"] unless $width && $height;
+    my $is_portrait = ($orientation =~ /\A(left|right)_/ ? 1:0) ^ ($width <= $height ? 1:0) ? 1:0;
 
     [200, "OK", $is_portrait, {'cmdline.exit_code' => $is_portrait ? 0:1, 'cmdline.result' => ''}];
 }
 
 $SPEC{image_is_landscape} = {
     v => 1.1,
-    summary => 'Return exit code 0 if media is landscape',
+    summary => 'Return exit code 0 if image is landscape',
     description => <<'_',
-
-Portrait is defined as having 'rotate' metadata of 90 or 270. Otherwise, media
-is assumed to be 'landscape'.
 
 _
     args => {
@@ -103,21 +98,22 @@ _
 sub image_is_landscape {
     my %args = @_;
 
-    my $res = media_info(media => [$args{media}], backend=>$args{backend});
+    my $res = image_info(files => [$args{file}]);
     return $res unless $res->[0] == 200;
 
-    my $rotate = $res->[2]{rotate} // 0;
-    my $width  = $res->[2]{video_width}  // $res->[2]{width};
-    my $height = $res->[2]{video_height} // $res->[2]{height};
-    return [412, "Can't determine video width x height"] unless $width && $height;
-    my $is_landscape = ($rotate == 90 || $rotate == 270 ? 1:0) ^ ($width <= $height ? 1:0) ? 0:1;
+    my $orientation = $res->[2]{Orientation} // '';
+    my $width  = $res->[2]{width};
+    my $height = $res->[2]{height};
+    return [412, "Can't determine image orientation"] unless $orientation;
+    return [412, "Can't determine image width x height"] unless $width && $height;
+    my $is_landscape = ($orientation =~ /\A(left|right)_/ ? 1:0) ^ ($width <= $height ? 1:0) ? 0:1;
 
     [200, "OK", $is_landscape, {'cmdline.exit_code' => $is_landscape ? 0:1, 'cmdline.result' => ''}];
 }
 
 $SPEC{image_orientation} = {
     v => 1.1,
-    summary => "Return orientation of image ('portrait' or 'landscape')",
+    summary => "Return orientation of image",
     args => {
         %arg0_file,
     },
